ba549e5b1f3f8db0b16eae71252ad39e1c706297
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-11-24 07:49:18 +0100

    test: finish after tests have run by default

--- projects/ngx-mat-timepicker-repo/karma.conf.js
+++ projects/ngx-mat-timepicker-repo/karma.conf.js
@@ -39,7 +39,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
     restartOnFileChange: true
   });
 };
