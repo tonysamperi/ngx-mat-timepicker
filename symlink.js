cd309103708a1b6328527e3488d1c36014eb9673
Tony Samperi <github@tonysamperi.it>
2022-11-25 09:49:32 +0100

    Improve logs on symlink.js

--- symlink.js
+++ symlink.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
-const {join} = require("path");
+const {join, toNamespacedPath} = require("path");
 const pkg = require("./package.json");
 
 (function createSymlink() {
     const v = pkg.version.split(".")[0];
     const nodeModulesPath = join(__dirname, "node_modules");
+    const targetPath = join("_node_modules", "node_modules_" + v);
     if (fs.existsSync(nodeModulesPath)) {
         fs.unlinkSync(nodeModulesPath);
     }
-    fs.symlink(join(__dirname, "_node_modules", "node_modules_" + v), nodeModulesPath, 'dir', (err) => {
+    fs.symlink(join(__dirname, targetPath), nodeModulesPath, 'dir', (err) => {
         if (err) {
             console.log(err);
         }
         else {
-            console.log("Symlink created");
+            console.log(`Symlink created: /node_modules => /${targetPath}`);
             console.log("Symlink is a directory:",
                 fs.statSync(nodeModulesPath).isDirectory()
             );
