f77f6a4e866689f937be924fa0f3e5d608007917
Tony Samperi <github@tonysamperi.it>
2023-06-22 19:49:48 +0200

    v16.0.2 * Fix change Hostlistener behaviour and add check after parse to handle invalid parsed DateTime (fixes [#70](https://github.com/tonysamperi/ngx-mat-timepicker/issues/70))

--- projects/ngx-mat-timepicker-repo/src/app/components/test/test.component.ts
+++ projects/ngx-mat-timepicker-repo/src/app/components/test/test.component.ts
@@ -1,6 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
+import {FormControl, Validators} from "@angular/forms";
 //
-import {NgxMatTimepickerLocaleService} from "ngx-mat-timepicker";
+import {NgxMatTimepickerComponent, NgxMatTimepickerLocaleService} from "ngx-mat-timepicker";
 //
 import {NgxMatTimepickerDemoComponent} from "../demo/demo.component";
 
@@ -13,8 +14,26 @@ import {NgxMatTimepickerDemoComponent} from "../demo/demo.component";
 })
 export class NgxMatTimepickerTestComponent extends NgxMatTimepickerDemoComponent {
 
+    formControlItem: FormControl = new FormControl("", [Validators.pattern(/([0-9]|[1-2]\d):[0-5]\d/)]);
+
+    @ViewChild("timepicker") timepicker: NgxMatTimepickerComponent;
+
     constructor(localeOverrideSrv: NgxMatTimepickerLocaleService) {
         super(localeOverrideSrv);
+    }
+
+    onClear() {
+        this.formControlItem.setValue(null);
+    }
+
+    onFieldBlur(): void {
+        this.formControlItem.valid && this.pickerFreeInput.updateTime(this.formControlItem.value);
+    }
+
+    openFromIcon(timepicker: { open: () => void }) {
+        if (!this.formControlItem.disabled) {
+            timepicker.open();
+        }
     }
 
 }
